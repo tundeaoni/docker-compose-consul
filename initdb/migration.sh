264f9d1b32d8a733f62cb275d61bbebecfae4166
Olatunde Alex-Oni <tunde.alexoni@yahoo.com>
2018-11-24 15:40:31 +0100

    ch: remove app volume mapping

--- initdb/migration.sh
+++ initdb/migration.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
-echo "Table greeting"
 psql  -U greeter -c "CREATE TABLE greeting ( message TEXT NOT NULL);"
-
-echo "seed"
 psql  -U greeter -c "INSERT INTO greeting (message) VALUES ('Hello, world');"
